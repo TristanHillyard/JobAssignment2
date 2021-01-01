375f203d9f40f15460840dbac6a5883a6e546079
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-01 13:51:18 -0700

    Add project files.

---
+++ JobAssignment2/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobAssignment2
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ JobAssignment2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JobAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+        }
+    }
+}
