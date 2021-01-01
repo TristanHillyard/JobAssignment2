37326d82634c9d540abc5fee21fe5659367244a7
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-01 16:12:30 -0700

    overloading == operator

--- JobAssignment2/Employee.cs
+++ JobAssignment2/Employee.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace JobAssignment2
 {
     public class Employee : Person, IQuittable
     {
+        public int EmployeeId { get; set; }
+
         public override void SayName()
         {
             base.SayName();
@@ -13,6 +16,20 @@ namespace JobAssignment2
         public void Quit()
         {
             Console.WriteLine(firstName + lastName + "quits");
+        }
+        public static bool operator ==(Employee employee, Employee employee1)
+        {
+            if (employee.EmployeeId == employee1.EmployeeId)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Employee employee, Employee employee1)
+        {
+            if (employee.EmployeeId == employee1.EmployeeId)
+                return false;
+            else
+                return true;
         }
     }
 }
--- JobAssignment2/Program.cs
+++ JobAssignment2/Program.cs
@@ -9,8 +9,16 @@ namespace JobAssignment2
             Employee employee = new Employee();
             employee.firstName = "Sample";
             employee.lastName = "Student";
+            employee.EmployeeId = 1;
             employee.SayName();
             employee.Quit();
+
+            Employee employee1 = new Employee();
+            employee.firstName = "Tristan";
+            employee.lastName = "Hillyard";
+            employee.EmployeeId = 2;
+            employee.SayName();
+            Console.WriteLine(employee.EmployeeId == employee1.EmployeeId);
         }
     }
 }
