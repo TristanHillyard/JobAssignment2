2aa79bf10af50c2bfb59aceba580a273062bf292
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-01 22:02:52 -0700

    generic parameter

--- JobAssignment2/Employee.cs
+++ JobAssignment2/Employee.cs
@@ -5,8 +5,9 @@ using System.Linq;
 
 namespace JobAssignment2
 {
-    public class Employee : Person, IQuittable
+    public class Employee<T> : Person, IQuittable
     {
+        public List<T> Things { get; set; }
         public int EmployeeId { get; set; }
 
         public override void SayName()
@@ -17,19 +18,19 @@ namespace JobAssignment2
         {
             Console.WriteLine(firstName + lastName + "quits");
         }
-        public static bool operator ==(Employee employee, Employee employee1)
-        {
-            if (employee.EmployeeId == employee1.EmployeeId)
-                return true;
-            else
-                return false;
-        }
-        public static bool operator !=(Employee employee, Employee employee1)
-        {
-            if (employee.EmployeeId == employee1.EmployeeId)
-                return false;
-            else
-                return true;
-        }
+        //public static bool operator ==(Employee employee, Employee employee1)
+        //{
+        //    if (employee.EmployeeId == employee1.EmployeeId)
+        //        return true;
+        //    else
+        //        return false;
+        //}
+        //public static bool operator !=(Employee employee, Employee employee1)
+        //{
+        //    if (employee.EmployeeId == employee1.EmployeeId)
+        //        return false;
+        //    else
+        //        return true;
+        //}
     }
 }
